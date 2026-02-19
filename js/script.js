ad94f0d32e6cdd34068b48fa3b9cf59c511a73dd
SURAMBAW <jowwwnnnsss.com>
2026-02-19 13:00:17 +0800

    WSAT-M4: JavaScript interactivity added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 console.log("submitBtn")
 
 const themeToggle = document.getElementById("themeToggle");
-    themeToggle.addEventListener("click",function(){
-    document.body.classList.toggle("dark-mode");
+themeToggle.addEventListener("click",function(){
+document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")){
+        themeToggle.textContent = "Toggle Light Mode";
+    } else {
+        themeToggle.textContent = "Toggle Dark Mode";
+    }
 });
 
 
