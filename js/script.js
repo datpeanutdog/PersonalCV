838bdfd18bc92f7ae33f438f881f2bc8dd40c774
SURAMBAW <jowwwnnnsss.com>
2026-02-19 12:16:23 +0800

    WSAT-M4: JavaScript interactivity added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 console.log("submitBtn")
 
-const submitBtn = document.getElementById("submitBtn");
-submitBtn.addEventListener("click",function(event){
-    event.preventDefault();
-    alert("Thank you! Your message has been received.");
-});
-
 const themeToggle = document.getElementById("themeToggle");
     themeToggle.addEventListener("click",function(){
     document.body.classList.toggle("dark-mode");
@@ -26,5 +20,6 @@ submitBtn.addEventListener("click",function(event){
     alert("Please fill in all required fields.");
     } else{
     alert("Form submitted successfully!");
+    alert("Thank you! Your message has been received.");
     }
 });
